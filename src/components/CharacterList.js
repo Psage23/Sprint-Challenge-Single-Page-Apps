a31031df978bc4f2af293bf92c130029ee3f0f91
Psage23 <pamelasage3@gmail.com>
2019-11-15 11:31:12 -0700

    almost Mvp still working on some issues

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,61 +1,38 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
-const CharacterList=props => {
-  // TODO: Add useState to track data from useEffect
+export default function CharacterList() {
+  //TODO: Add useState to track datat from useEffect
+
   const [characters, setCharacters] = useState([]);
+  const [filter, setFilter] = useState([])
+
+  const search = charArr => {
+    setFilter(charArr)
+  };
 
   useEffect(() => {
-    const getCharacter = () => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios.get(`http://rickandmortyapi.com/api/character/`)
+    Axios.get(`https://rickandmortyapi.com/api/character/`)
     .then(response => {
+      console.log(response.data.results);
       setCharacters(response.data.results);
-      console.log(response.data.results)
-    })
-    .catch(error => {
-      console.error(error);
-    })
-    }
-    getCharacter();
+      setFilter(response.data.results);
+    });
   }, []);
 
   return (
     <section className="character-list">
-        {characters.map((character, index) => {
-          return(
-            <CharacterDetails
-            key = {index}
-            name = {character.name}
-            status = {character.status}
-            species = {character.species}
-            />
-          )
-        })}
+      <h2>Character List</h2>
+      <Link className="main-buttons" to={"/"}>
+        Home
+      </Link>
+      <SearchForm search={search} characters={characters} />
+      {filter.map(char => {
+        return <CharacterCard key={char.id} character={char}/>
+      })}
     </section>
-  );
-}
-
-function CharacterDetails({character}){
-  const {name, status, species } = character;
-  return (
-    <div className="name-card">
-      <h2>{name}</h2>
-      <div className="status">
-        Status: <em>{status}</em>
-      </div>
-      <div className="species">
-        Species: <strong>{species}</strong>
-      </div>
-
-      {species.map(e => (
-        <div key={species} className="character-specie">
-          {species}
-        </div>
-      ))}
-    </div>
   )
 }
-
-export default CharacterList;
